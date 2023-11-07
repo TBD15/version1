173871a1acdfe715e2479a186c640ab31cbd6e1d
Katherine Marrow <kmcromer1@gmail.com>
2023-11-06 22:28:21 -0500

    posts

--- client/pages/profile/profile.tsx
+++ client/pages/profile/profile.tsx
@@ -20,67 +20,92 @@ interface Post {
   imageURL: string;
 }
 
+// example data:
+const UserData = {
+  profilePic:
+    "https://www.akc.org/wp-content/uploads/2017/11/Great-Pyrenees-puppy-sittin-in-a-chair-outdoors.jpg",
+  name: "Luna",
+  username: "@luna",
+  firstName: "Luna",
+};
+const posts = [
+  {
+    id: "1",
+    author: "@luna",
+    imageURL:
+      "https://media.istockphoto.com/id/533893554/photo/adorable-great-pyrenees-puppy.jpg?s=612x612&w=0&k=20&c=-qnIegZ9kyjlKnkj0_4BXxyKA_fUcSieVaxLPGCQgFk=",
+  },
+  {
+    id: "2",
+    author: "@luna",
+    imageURL:
+      "https://t3.ftcdn.net/jpg/05/68/57/80/360_F_568578090_tGUveLYBZweusJjdLtCcnQfEdMRIMoX2.jpg",
+  },
+];
+
 let filterCardsByAuthor = false;
 //ProfilePage is the React component. It's a functional component
 //ProfilePage is a functional component declared using an arrow function. It doesn't receive any props, and it defines the component logic and JSX rendering within its body.
 const ProfilePage = () => {
   //useState is used to manage the component's state, specifically user, which is used to store user data.
-  const [user, setUser] = useState<UserData | undefined>(undefined);
+  //const [user, setUser] = useState<UserData | undefined>(undefined);
 
   //useState to manage component state, specifically posts, which is used to store post data.
-  const [posts, setPosts] = useState<any[]>([]);
+  //const [posts, setPosts] = useState<any[]>([]);
   //useEffect hook is used to fetch user data from the backend when the component is mounted.
-  useEffect(() => {
-    //if there is no user, fetch the user data from the backend
-    if (!user) {
-      //fetch the user data from the backend
-      fetch("/api/auth/user", { method: "GET" })
-        //if the response is 200, then return the data in json format
-        .then((d) => {
-          if (d.status !== 200) {
-            console.log(`Res status was not 200`);
-          }
-          console.log("hit no user on profil4 page");
-          return d.json();
-        })
-        //if there is an error, then log the error
-        .then((d) => {
-          // Set the user state
-          setUser(d);
-        })
-        .catch((e) => {
-          console.log("hit no user on profile page");
-          console.log("Error occured: ", e);
-        });
-    }
-  });
+  //   useEffect(() => {
+  //     //if there is no user, fetch the user data from the backend
+  //     if (!user) {
+  //       //fetch the user data from the backend
+  //       fetch("/api/auth/user", { method: "GET" })
+  //         //if the response is 200, then return the data in json format
+  //         .then((d) => {
+  //           if (d.status !== 200) {
+  //             console.log(`Res status was not 200`);
+  //           }
+  //           console.log("hit no user on profil4 page");
+  //           return d.json();
+  //         })
+  //         //if there is an error, then log the error
+  //         .then((d) => {
+  //           // Set the user state
+  //           setUser(d);
+  //         })
+  //         .catch((e) => {
+  //           console.log("hit no user on profile page");
+  //           console.log("Error occured: ", e);
+  //         });
+  //     }
+  //   });
 
   //useEffect hook is used to fetch post data from the backend when the component is mounted.
-  useEffect(() => {
-    //if there are no posts, fetch the posts data from the backend
-    if (posts.length === 0) {
-      //fetch the posts data from the backend
-      fetch("/api/posts", { method: "GET" })
-        //if the response is 200, then return the data in json format
-        .then((d) => {
-          if (d.status !== 200) {
-            console.log(`Res status was not 200`);
-          }
-          return d.json();
-        })
-        //if there is an error, then log the error
-        .then((d) => {
-          // Set the posts state
-          //filter to only show posts by the user
-          if (filterCardsByAuthor) {
-            setPosts(d.filter((post: Post) => post.author === user?.username));
-          }
-        })
-        .catch((e) => {
-          console.log("Error occured: ", e);
-        });
-    }
-  });
+  //   useEffect(() => {
+  //     //if there are no posts, fetch the posts data from the backend
+  //     if (posts.length === 0) {
+  //       //fetch the posts data from the backend
+  //       fetch("/api/posts", { method: "GET" })
+  //         //if the response is 200, then return the data in json format
+  //         .then((d) => {
+  //           if (d.status !== 200) {
+  //             console.log(`Res status was not 200`);
+  //           }
+  //           return d.json();
+  //         })
+  //         //if there is an error, then log the error
+  //         .then((d) => {
+  //           // Set the posts state
+  //           //filter to only show posts by the user
+  //           if (filterCardsByAuthor) {
+  //             setPosts(d.filter((post: Post) => post.author === user?.username));
+  //           }
+  //         })
+  //         .catch((e) => {
+  //           console.log("Error occured: ", e);
+  //         });
+  //     }
+  //   });
+
+  //example set
 
   // This will be used to delete posts
 
@@ -94,22 +119,29 @@ const ProfilePage = () => {
   //It appears to be a profile page with an avatar, username, and a section for posts.
   return (
     <body>
-      <div className={styles.pinkHeader}>
-        <div className={styles.header}>Profile</div>
-      </div>
-      <div className={styles.background}>
-        <div className={styles.profilesection}>
-          <img
-            className={styles.profilePic}
-            src={
-              "https://www.akc.org/wp-content/uploads/2017/11/Great-Pyrenees-puppy-sittin-in-a-chair-outdoors.jpg"
-            }
-            alt='Pic'
-          />
+      <div>
+        <div className={styles.pinkHeader}>
+          <div className={styles.header}>Profile</div>
+        </div>
+        <div className={styles.background}>
+          <div className={styles.profilesection}>
+            <img
+              className={styles.profilePic}
+              src={
+                "https://www.akc.org/wp-content/uploads/2017/11/Great-Pyrenees-puppy-sittin-in-a-chair-outdoors.jpg"
+              }
+              alt='Pic'
+            />
+            {/* //real
+            <div className={styles.profilename}>{user?.name || "kate"}</div>
+            //example */}
+            <div className={styles.profilename}>
+              {UserData?.firstName || "kate"}
+            </div>
 
-          <div className={styles.profilename}>{user?.name || "kate"}</div>
-          <div className={styles.profileusername}>
-            {user?.username || "@kate"}
+            <div className={styles.profileusername}>
+              {UserData?.username || "@kate"}
+            </div>
           </div>
         </div>
         <div className={styles.postsSection}>Posts</div>
