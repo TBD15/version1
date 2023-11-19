88b18124b392b615550092c32086b3f2d28651ce
Katherine Marrow <kmcromer1@gmail.com>
2023-11-19 12:38:56 -0500

    updated cypress and started jest confi

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+// module.exports = {
+//   preset: "ts-jest/presets/js-with-ts",
+//   testEnvironment: "jest-environment-jsdom",
+//   // setupFilesAfterEnv: [
+//   //   "@testing-library/jest-dom/extend-expect",
+//   //   "setupJest.js",
+//   // ],
+//   moduleNameMapper: {
+//     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+//       "<rootDir>/mocks/fileMock.js",
+//     "\\.(css)$": "<rootDir>/mocks/fileMock.js",
+//   },
+// };
+
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+};
+
+export default config;
